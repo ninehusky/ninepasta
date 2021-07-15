096e62ad2a0a11910e9016c09b730488531859ac
Andrew Cheung <acheung8@uw.edu>
2021-07-15 02:56:05 -0700

    Started login middleware

--- server/src/users/index.js
+++ server/src/users/index.js
@@ -5,6 +5,8 @@ const router = express.Router();
 
 const { validateUser, User } = require("./schema");
 
+const { auth } = require("./middleware");
+
 // Create
 router.post("/", async (req, res, next) => {
   try {
@@ -48,36 +50,71 @@ router.get("/login", async (req, res, next) => {
     const existingUser = await User.findOne({
       username: req.body.username,
     }).select("+password");
-    const result = await bcrypt.compare(
+    if (!existingUser) {
+      throw new Error("Incorrect username/password!");
+    }
+    const valid = await bcrypt.compare(
       req.body.password,
       existingUser.password
     );
-    res.json(result);
+    if (!valid) {
+      res.statusCode = 403; // Unauthorized
+      throw new Error("Incorrect username/password!");
+    }
+    const token = jwt.sign(
+      {
+        uid: existingUser._id,
+      },
+      process.env.JWT_SECRET,
+      {
+        expiresIn: "1d",
+      }
+    );
+    res.json({
+      token: token,
+    });
   } catch (error) {
     next(error);
   }
 });
 
 // Update
-router.patch("/", async (req, res, next) => {
+router.patch("/:id", auth, async (req, res, next) => {
   try {
+    const id = req.params.id;
+    if (!id) {
+      throw new Error("ID must be included!");
+    }
     await validateUser(req.body);
-    const existingUser = await User.findOne({
-      username: req.body.username,
-    });
+    const existingUser = await User.findById(id);
     if (!existingUser) {
       throw new Error("A user with the given name does not exist!");
+    } else if (existingUser._id != id) {
+      console.log(existingUser.username, req.body.username);
+      console.log(existingUser._id, id);
+      throw new Error("You are not logged in as this user!");
     }
+
+    // now check that the user isn't already taken
+    const dupe = await User.findOne({
+      username: req.body.username,
+    });
+    if (dupe) {
+      throw new Error("A user with that username already exists!");
+    }
+
     const saltRounds = 10;
     const hashedPass = bcrypt.hashSync(req.body.password, saltRounds);
     const userInfo = {
       username: req.body.username,
       password: hashedPass,
     };
-    const user = await User.findByIdAndUpdate(existingEntry._id, userInfo, {
+
+    const user = await User.findByIdAndUpdate(existingUser._id, userInfo, {
       useFindAndModify: false,
       upsert: false,
     });
+
     // please dont delete this lol. really please dont
     user.password = undefined;
     res.json(user);
@@ -87,5 +124,9 @@ router.patch("/", async (req, res, next) => {
 });
 
 // Destroy
+
+// check that user isn't already in database
+// hash password given object
+const insertUser = () => {};
 
 module.exports = router;
---
+++ server/src/users/middleware.js
@@ -0,0 +1,22 @@
+const jwt = require("jsonwebtoken");
+
+const auth = (req, res, next) => {
+  try {
+    let token = req.header("Authorization");
+    if (!token || !token.split(" ")[1]) {
+      res.statusCode = 401;
+      throw new Error("Not logged in.");
+    }
+    token = token.split(" ")[1];
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.uid = decoded;
+    return next();
+  } catch (err) {
+    console.error(err);
+    next(err);
+  }
+};
+
+module.exports = {
+  auth,
+};
