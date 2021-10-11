08c80fb849ecd425678914006e8dc366e04057ed
Andrew Cheung <acheung8@uw.edu>
2021-10-10 20:55:51 -0700

    Undid cookie settings

--- server/src/app.js
+++ server/src/app.js
@@ -46,7 +46,7 @@ app.use(
     saveUninitialized: false,
     secret: process.env.ACCESS_TOKEN_SECRET,
     cookie: {
-      sameSite: "none",
+      sameSite: "lax",
       maxAge: 1000 * 60 * 60 * 24 * 7,
       httpOnly: true,
       secure: true,
